fa83a917479bfdad0859ed9d8b9d9c8f566115e1
00Rohith00 <144118006+00Rohith00@users.noreply.github.com>
2024-01-04 16:07:58 +0530

    Add files via upload

---
+++ zip.js
@@ -0,0 +1,23 @@
+// converting upload folder into zip folder with unique name (server-side).
+
+const AdmZip = require("adm-zip")
+
+var filename  = Date.now()+".zip"
+
+async function createZipArchive(){
+ 
+try 
+{
+    const zip = new AdmZip();
+    const outputFile = filename;
+    zip.addLocalFolder("./upload");
+    zip.writeZip(outputFile);
+    console.log(`Created ${outputFile} successfully...`);
+} 
+catch (e) 
+{
+    console.log(`Something went wrong. ${e}`);
+}
+
+}
+createZipArchive();

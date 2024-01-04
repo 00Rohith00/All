d0f564afd11febbcd497722e409a87cc20dbf51b
00Rohith00 <144118006+00Rohith00@users.noreply.github.com>
2024-01-04 18:09:38 +0530

    Update

--- selenium2.js
+++ selenium2.js
@@ -1,62 +1,50 @@
+import chrome from 'chromedriver';
+import {Builder , By} from 'selenium-webdriver';
 
-import webdriver from 'selenium-webdriver';
-require("chromedriver");
-
-const verifyDoctorRegisterNumber = async (year, number) => {
-  let flag = 0;
-  const driver = await new webdriver.Builder().forBrowser("chrome").build();
-
-  await driver.get(
-    "https://www.nmc.org.in/information-desk/indian-medical-register/"
-  );
-
-  await driver
-    .findElement(webdriver.By.xpath('//a[@href="#registrationNumber"]'))
-    .click();
-
-  await driver.manage().setTimeouts({ implicit: 1000 });
-
-  await driver
-    .findElement(webdriver.By.xpath('//input[@id="doct_regdNo"]'))
-    .sendKeys(number);
-
-  await driver
-    .findElement(webdriver.By.xpath('//button[@id="doctor_regdno_details"]'))
-    .click();
-
-  await driver.manage().setTimeouts({ implicit: 5000 });
-
-  const body = await driver
-    .findElement(
-      webdriver.By.xpath(
-        '//table[@class="table table-bordered no-footer dataTable"]'
-      )
-    )
-    .findElement(webdriver.By.xpath("tbody"))
-    .findElements(webdriver.By.xpath("tr"));
-
-  for (var n of body) {
-    const list = await n.findElements(webdriver.By.xpath("td"));
-
-    const date = await list[1].getText();
-
-    if (date === year) {
-      flag++;
-      list.forEach(async (x) => {
-        ans = await x.getText();
-
-        console.log(ans);
-      });
-      break;
-    }
-  }
-
-  if (flag == 0) {
-    console.log("false");
-  }
-
-  return flag
-  //driver.close();
+const verifyDoctorRegisterNumber = async (year, number,medicalCouncil) => {
+    let flag = 0;
+  
+    const driver = await new Builder().forBrowser("chrome").build();
+  
+    await driver.get("https://www.nmc.org.in/information-desk/indian-medical-register/");
+  
+    await driver.findElement(By.xpath('//a[@href="#registrationNumber"]')).click();
+  
+    await driver.manage().setTimeouts({ implicit: 1000 });
+  
+    await driver.findElement(By.xpath('//input[@id="doct_regdNo"]')).sendKeys(number);
+  
+    await driver.findElement(By.xpath('//button[@id="doctor_regdno_details"]')).click();
+  
+    await driver.manage().setTimeouts({ implicit: 5000 });
+  
+    const body = await driver.findElement(By.xpath('//table[@class="table table-bordered no-footer dataTable"]')).findElement(By.xpath("tbody")).findElements(By.xpath("tr"));
+  
+    for (var n of body)
+     {
+            const list = await n.findElements(By.xpath("td"));
+    
+            const period = await list[1].getText();
+    
+            const state = await list[3].getText();
+    
+            if (period === year  && state === medicalCouncil) 
+            {
+                flag++;
+                list.forEach(async (x) => {
+                    var ans = await x.getText();
+                    console.log(ans);
+                });
+                break;
+            }
+      }
+     if (flag == 0) { console.log("false"); }
+  
+     return flag
+  
+     //driver.close();
 };
+
+verifyDoctorRegisterNumber("1960" , "12345", "Madras Medical Council")
 
 export { verifyDoctorRegisterNumber };

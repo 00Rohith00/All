d92a28ccb00e289ddec7afc9818a2645ea4208de
00Rohith00 <144118006+00Rohith00@users.noreply.github.com>
2024-01-04 18:10:54 +0530

    Update

--- zip.js
+++ zip.js
@@ -1,23 +1,24 @@
 // converting a folder into zip folder with unique name
 
-// Required { node } { adm-zip }
+// Required packages: { node } { adm-zip }
 
 const AdmZip = require("adm-zip")
 
 var filename  = Date.now()+".zip"
 
-async function createZipArchive(){
-try 
+async function createZipArchive()
 {
-    const zip = new AdmZip();
-    const outputFile = filename;
-    zip.addLocalFolder("./upload");
-    zip.writeZip(outputFile);
-    console.log(`Created ${outputFile} successfully...`);
-} 
-catch (e) 
-{
-    console.log(`Something went wrong. ${e}`);
-}
+        try 
+        {
+            const zip = new AdmZip();
+            const outputFile = filename;
+            zip.addLocalFolder("./upload");
+            zip.writeZip(outputFile);
+            console.log(`Created ${outputFile} successfully...`);
+        } 
+        catch (e) 
+        {
+            console.log(`Something went wrong. ${e}`);
+        }
 }
 createZipArchive();
